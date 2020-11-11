cac7e570c91833e2e75313e012036d4d4c2f29eb
domingobishop <domingobishop@gmail.com>
2020-11-11 14:06:24 +0000

    reformat code

--- s3.tf
+++ s3.tf
@@ -1,19 +1,19 @@
 resource "aws_s3_bucket" "cdn" {
-  bucket = var.cdn_bucket_name
-  acl    = "public-read"
+    bucket = var.cdn_bucket_name
+    acl    = "public-read"
 
-  website {
-    index_document = "index.html"
-    error_document = "404.html"
-  }
+    website {
+        index_document = "index.html"
+        error_document = "404.html"
+    }
 
-  tags = {
-    Name        = var.cdn_bucket_name
-    Environment = var.environment
-    Service     = var.service
-    Owner       = var.owner
-    CreatedBy   = var.created_by
-    CostCentre  = var.cost_centre
-    Terraform   = "True"
-  }
+    tags = {
+        Name        = var.cdn_bucket_name
+        Environment = var.environment
+        Service     = var.service
+        Owner       = var.owner
+        CreatedBy   = var.created_by
+        CostCentre  = var.cost_centre
+        Terraform   = "True"
+    }
 }
