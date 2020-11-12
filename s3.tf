9b0f4686d53531b727691786221f1e678e115eba
domingobishop <domingobishop@gmail.com>
2020-11-12 11:29:07 +0000

    type

--- s3.tf
+++ s3.tf
@@ -14,6 +14,6 @@ resource "aws_s3_bucket" "cdn" {
         Owner       = var.owner
         CreatedBy   = var.created_by
         CostCentre  = var.cost_centre
-        Terraform   = "True"
+        Terraform   = "true"
     }
 }
