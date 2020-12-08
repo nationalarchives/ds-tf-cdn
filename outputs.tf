7c7b8e3e1d7bf41a68b19e7a3e02c85c08ef40ba
domingobishop <domingobishop@gmail.com>
2020-12-08 14:39:28 +0000

    output cloudfront_origin_access_identity_iam_arn arn

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,7 @@ output "cdn_s3_bucket_arn" {
 output "cdn_cloudfront_domain_name" {
     value = aws_cloudfront_distribution.cdn.domain_name
 }
+
+output "cdn_cloudfront_origin_access_identity_iam_arn" {
+    value = aws_cloudfront_origin_access_identity.cdn.iam_arn
+}
